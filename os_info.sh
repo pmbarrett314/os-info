71622fa48e89d0a2bdaf6dd9a9bbd708b07f92d8
Paul Barrett <pmbarrett314@gmail.com>
2017-03-28 14:20:35 -0500

    fixed errors from shellcheck

--- os_info.sh
+++ os_info.sh
@@ -8,79 +8,94 @@
 
 GetVersionFromFile()
 {
-	VERSION=`cat $1 | tr "\n" ' ' | sed s/.*VERSION.*=\ // `
+	VERSION="$(tr "\n" ' ' < cat "$1" | sed s/.*VERSION.*=\ // )"
 }
 
-case $OSTYPE in
-	solaris*) 
-		OS="Solaris"
-		;;
-  	darwin*)  
-		OS="macos"
-		;; 
-  	linux*)   
-		OS="Linux"
-		;;
-  	bsd*)     
-		OS="bsd" 
-		;;
-  	msys*)    
-		OS="msys" 
-		;;
-	cygwin*)    
-		OS="cygwin" 
-		;;
-  	*)    
-  		OS=`uname -s`    
-		echo "unknown: $OSTYPE"
-		echo "using OS value $OS"
-		;;
-esac
+# shellcheck disable=SC2039
+if [ -z "${OSTYPE+x}" ]; then
+	case $OSTYPE in
+		solaris*) 
+			OS="Solaris"
+			;;
+	  	darwin*)  
+			OS="macos"
+			;; 
+	  	linux*)   
+			OS="Linux"
+			;;
+	  	bsd*)     
+			OS="bsd" 
+			;;
+	  	msys*)    
+			OS="msys" 
+			;;
+		cygwin*)    
+			OS="cygwin" 
+			;;
+	  	*)    
+	  		OS=$(uname -s)
+			echo "unknown: $OSTYPE"
+			echo "using OS value $OS"
+			;;
+	esac
+else
+	OS=$(uname -s)
+	echo "unknown: $OSTYPE"
+	echo "using OS value $OS"
+fi
 
-REV=`uname -r`
-MACH=`uname -m`
+REV=$(uname -r)
+MACH=$(uname -m)
 
 
 if [ "${OS}" = "SunOS" ] ; then
 	OS=Solaris
-	ARCH=`uname -p`	
-	OSSTR="${OS} ${REV}(${ARCH} `uname -v`)"
+	ARCH=$(uname -p)
+	OSSTR="${OS} ${REV}(${ARCH} $(uname -v)"
 elif [ "${OS}" = "AIX" ] ; then
-	OSSTR="${OS} `oslevel` (`oslevel -r`)"
+	OSSTR="${OS} $(oslevel) ($(oslevel -r)"
 elif [ "${OS}" = "Linux" ] ; then
-	KERNEL=`uname -r`
+	KERNEL=$(uname -r)
 	if [ -f /etc/redhat-release ] ; then
 		DIST='RedHat'
-		PSUEDONAME=`cat /etc/redhat-release | sed s/.*\(// | sed s/\)//`
-		REV=`cat /etc/redhat-release | sed s/.*release\ // | sed s/\ .*//`
+		PSUEDONAME=$(sed s/.*\(// < /etc/redhat-release | sed s/\)//)
+		REV=$(sed s/.*release\ // < /etc/redhat-release | sed s/\ .*//)
 	elif [ -f /etc/SuSE-release ] ; then
-		DIST=`cat /etc/SuSE-release | tr "\n" ' '| sed s/VERSION.*//`
-		REV=`cat /etc/SuSE-release | tr "\n" ' ' | sed s/.*=\ //`
+		DIST=$(tr "\n" ' ' < /etc/SuSE-release | sed s/VERSION.*//)
+		REV=$(tr "\n" ' ' < /etc/SuSE-release| sed s/.*=\ //)
 	elif [ -f /etc/mandrake-release ] ; then
 		DIST='Mandrake'
-		PSUEDONAME=`cat /etc/mandrake-release | sed s/.*\(// | sed s/\)//`
-		REV=`cat /etc/mandrake-release | sed s/.*release\ // | sed s/\ .*//`
+		PSUEDONAME=$(sed s/.*\(// < /etc/mandrake-release | sed s/\)//)
+		REV=$(sed s/.*release\ // < /etc/mandrake-release | sed s/\ .*//)
 	elif [ -f /etc/debian_version ] ; then	
-		if [ `awk -F= '/DISTRIB_ID/ {print $2}' /etc/lsb-release` = "Ubuntu" ]; then
+		if [ "$(awk -F= '/DISTRIB_ID/ {print $2}' /etc/lsb-release)" = "Ubuntu" ]; then
 			DIST="Ubuntu"
 		else
-			DIST="Debian `cat /etc/debian_version`"
+			DIST="Debian $(cat /etc/debian_version)"
 			REV=""
 		fi
 	elif [ -f /etc/arch-release ] ; then
 		DIST="Arch"
 	fi
 	if [ -f /etc/UnitedLinux-release ] ; then
-		DIST="${DIST}[`cat /etc/UnitedLinux-release | tr "\n" ' ' | sed s/VERSION.*//`]"
+		DIST="${DIST}[$(tr "\n" ' ' < /etc/UnitedLinux-release | sed s/VERSION.*//)]"
 	fi
 
 	OSSTR="${OS} ${DIST} ${REV}(${PSUEDONAME} ${KERNEL} ${MACH})"
 fi
 
 get_distro() {
-	echo ${DIST}
+	echo "${DIST}"
 }
 
 get_os() {
-	echo ${OS}
+	echo "${OS}"
+}
+
+get_version(){
+	echo "${VERSION}"
+}
+
+get_osstr(){
+	echo "${OSSTR}"
 }
